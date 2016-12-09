6b94d78b6bf033fa7f78240726ed24a83ca683a1
JZorvan <jzorvan@gmail.com>
2016-12-08 19:45:06 -0800

    sample api call to OED works

--- Vocabulous/Scripts/app.js
+++ Vocabulous/Scripts/app.js
@@ -1,1 +1,30 @@
 ﻿var app = angular.module("Vocabulous", []);
+
+app.controller('DictionaryCtrl', function ($scope, $http) {
+
+    $scope.vocabulary = [];
+
+    $scope.getDictionary = function () {
+
+        var req = {
+            method: "GET",
+            url: "https://od-api.oxforddictionaries.com/api/v1/entries/en/failure",
+            headers: {
+                "app_id": "ffc1dd88",
+                "app_key": "fca7ccadd6301ca07d286ea20c59392e"
+            }
+        }
+
+        $http(req)
+                   .then(function (response) {
+                       console.log(response);
+                       $scope.vocabulary = response;
+                   }
+                   ,function (error) {
+                       console.log("failed!");
+                       debugger
+                    });
+    }
+
+    return $scope.vocabulary;
+});
